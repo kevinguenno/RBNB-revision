7f3b4f1a60c22469b8a88dc2bced22a346be49de
kevinguenno <kevin.guenno@gmail.com>
2023-03-27 11:15:23 +0200

    routes

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,3 +1,9 @@
 class Flat < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :flats
+  has_many :bookings, dependent: :destroy
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
