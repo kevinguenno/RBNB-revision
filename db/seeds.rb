2737ec02e9f70b31d7d43095ec148d3a9f5c0eb3
kevinguenno <kevin.guenno@gmail.com>
2023-03-28 14:14:06 +0200

    modif seed supression flat.user

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,12 @@ user_1 = User.create!({first_name: "Kevin", last_name: "Guenno"})
 user_2 = User.create!({first_name: "Laeticia", last_name: "Guerin"})
 
 flat_1 = Flat.new({name:"Appartement 1", description:"Super mer", address:"nantes", price: 150 })
-flat_1.user = user_1
 flat_1.save!
 
 flat_2 = Flat.new({name:"Appartement 2", description:"Super poubelle", address:"nantes", price: 100 })
-flat_2.user = user_1
 flat_2.save!
 
 flat_3 = Flat.new({name:"Appartement 3", description:"Pas de fenetre", address:"nantes", price: 50 })
-flat_3.user = user_2
 flat_3.save!
 
 puts "done"
